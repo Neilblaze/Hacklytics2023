7c5c2ef943fbc675da7aee823ce42822e19d9bf3
Gautham Gururajan <47530745+Gautham-G@users.noreply.github.com>
2023-02-12 03:38:02 -0500

    add transcribe

--- whisper/app.py
+++ whisper/app.py
@@ -1,10 +1,14 @@
 import flask
+from flask_cors import CORS
+from flask import jsonify
 from flask import request, render_template
 from tempfile import NamedTemporaryFile
 import whisper
 
 app = flask.Flask(__name__)
+CORS(app)
 model = whisper.load_model("small")
+# model = whisper.load_model("tiny")    # Shitass prediction
 
 def inference(audio):
     audio = whisper.load_audio(audio)
@@ -18,7 +22,11 @@ def inference(audio):
     
     return result.text
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=['GET', 'POST'])
+def root():
+    return "HealthifAI — Whisper Transcription Module  |  Server 200 OK!"
+
+@app.route("/view", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         file = request.files["file"]
@@ -29,6 +37,22 @@ def home():
         return render_template("template.html", transcription=transcription)
 
     return render_template("template.html")
+
+
+@app.route("/transcribe", methods=["GET", "POST"])
+def transcribe():
+    print("hello")
+    if request.method == "POST":
+        print(request.files, "req1")
+        file = request.files["file"]
+        with NamedTemporaryFile(suffix="mp3") as temp:
+            temp.write(file.read())
+            temp.seek(0)
+            transcription = inference(temp.name)
+            print("hi")
+        return jsonify({"transcription": transcription})
+
+    return jsonify({"error": "No file found in the request."})
 
 if __name__ == "__main__":
     app.run(debug=True)
